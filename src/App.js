63bd6696926f50e64333643348f3f62f555054f6
OahidZihad <oahidzihad1@gmail.com>
2022-05-30 02:30:02 +0600

    api problem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // fetch("http://3.237.196.207/api/videos", {
+    //   method: "GET",
+    //   mode: "no-cors",
+    //   redirect: "follow",
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Content-Type": "text/plain",
+    //   },
+    // })
+    //   .then((response) => response.text())
+    //   .then((result) => console.log("result", result))
+    //   .catch((error) => console.log("error", error));
+
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/plain",
+      },
+    };
+
+    fetch("http://3.237.196.207/api/videos", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log("result", result))
+      .catch((error) => console.log("error", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row gx-3 text-center">
+        <div class="col-md-8">
+          <div>Custom column padding</div>
+        </div>
+        <div class="col-md-4">
+          <div>Custom column padding</div>
+        </div>
+      </div>
     </div>
   );
 }
